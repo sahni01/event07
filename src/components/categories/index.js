032b925bd1195df4251350caf1aaecc3a3e55a30
NITESH <sahninitesh01@gmail.com>
2023-03-27 00:37:41 +0530

    update

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -3,7 +3,7 @@ import ChrismasImage from "../../assets/chrismas-decoration.jpg"
 
 export default function Categories(){
     return(
-        <div className=" container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 mx-auto px-5 my-10">
+        <div className=" container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-2 mx-auto px-5 my-10">
 
             <div className="border rounded-xl overflow-hidden relative transition-shadow">
                 <div className="w-full h-full">
