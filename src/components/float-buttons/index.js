268b41264a8ae9e6fb153d8ab35221dac3c03b47
NITESH <sahninitesh01@gmail.com>
2023-03-27 00:40:58 +0530

    updated

--- src/components/float-buttons/index.js
+++ src/components/float-buttons/index.js
@@ -7,7 +7,7 @@ export default function FloatingButton() {
     return (
         <div className="fixed z-50 bottom-4 w-full px-5 flex flex-row justify-between items-center">
             <Button onClick={()=>window.open("tel:+919871973507", "_self")} variant="contained" className="bg-accent text-primary px-5 py-3 rounded-full w-40" startIcon={<Call />} >Call Us</Button>
-            <Button onClick={()=>window.open("https://api.whatsapp.com/send/?phone=919871973507&text=Hi&type=phone_number&app_absent=0", "_blank")} variant="contained" className="bg-green-700 text-primary px-5 py-3 rounded-full w-40" startIcon={<WhatsApp />} >WhatsApp</Button>
+            <Button onClick={()=>window.open("https://api.whatsapp.com/send/?phone=919871973507&text=Hi&type=phone_number&app_absent=0", "_blank")} variant="contained" className="bg-green-700 text-primary px-5 py-3 rounded-full w-40 hover:bg-green-600" startIcon={<WhatsApp />} >WhatsApp</Button>
         </div>
     )
 }
